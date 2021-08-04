f08d11af1157998ef17571f9c9a91b15c68ebd1f
Chun Yang <chunyang@192-168-1-100.tpgi.com.au>
2021-08-04 22:09:31 +1000

    Asynchronous Spring event config

--- src/main/java/com/daniel/sample/eventhanddler/AsynchronousSpringEventConfig.java
+++ src/main/java/com/daniel/sample/eventhanddler/AsynchronousSpringEventConfig.java
@@ -1,7 +1,19 @@
 package com.daniel.sample.eventhanddler;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.event.ApplicationEventMulticaster;
+import org.springframework.context.event.SimpleApplicationEventMulticaster;
+import org.springframework.core.task.SimpleAsyncTaskExecutor;
 
 @Configuration
 public class AsynchronousSpringEventConfig {
+
+    @Bean(name="applicationEventMulticaster")
+    public ApplicationEventMulticaster simpleApplicationEventMultiCaster() {
+        SimpleApplicationEventMulticaster eventMulticaster = new SimpleApplicationEventMulticaster();
+        eventMulticaster.setTaskExecutor(new SimpleAsyncTaskExecutor());
+        return eventMulticaster;
+    }
+
 }
